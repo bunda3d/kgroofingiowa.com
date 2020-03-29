48d1fd144f4f850cb33a1139858721ae58b014f2
github <14361990+bunda3d@users.noreply.github.com>
2020-03-29 15:43:51 -0500

    update Material

--- kgr/src/app/modules/sidenav/sidenav.module.ts
+++ kgr/src/app/modules/sidenav/sidenav.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidenavComponent } from './sidenav.component';
-import { MatSidenavModule, MatSlideToggleModule, MatListModule } from '@angular/material';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
