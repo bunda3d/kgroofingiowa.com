e9f0509104635628787dbb1550df3bc41521d9fb
github <14361990+bunda3d@users.noreply.github.com>
2020-05-18 19:53:45 -0500

    tree shaking and build

--- kgr/src/app/app-routing.module.ts
+++ kgr/src/app/app-routing.module.ts
@@ -2,39 +2,38 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
-  },
-  {
-    path: 'about',
-    loadChildren: () => import('./modules/about/about.module').then(m => m.AboutModule)
-  },
-  {
-    path: 'contact',
-    loadChildren: () => import('./modules/contact/contact.module').then(m => m.ContactModule)
-  },
-  {
-    path: 'financing',
-    loadChildren: () => import('./modules/financing/financing.module').then(m => m.FinancingModule)
-  },
-  {
-    path: 'kitchensink',
-    loadChildren: () => import('./modules/kitchensink/kitchensink.module').then(m => m.KitchensinkModule)
-  },
-  {
-    path: 'projects',
-    loadChildren: () => import('./modules/projects/projects.module').then(m => m.ProjectsModule)
-  },
-  {
-    path: '',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  }
+	{
+		path: 'home',
+		loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+	},
+	{
+		path: 'about',
+		loadChildren: () => import('./modules/about/about.module').then(m => m.AboutModule)
+	},
+	{
+		path: 'contact',
+		loadChildren: () =>
+			import('./modules/contact/contact.module').then(m => m.ContactModule)
+	},
+	{
+		path: 'financing',
+		loadChildren: () =>
+			import('./modules/financing/financing.module').then(m => m.FinancingModule)
+	},
+	{
+		path: 'projects',
+		loadChildren: () =>
+			import('./modules/projects/projects.module').then(m => m.ProjectsModule)
+	},
+	{
+		path: '',
+		redirectTo: '/home',
+		pathMatch: 'full'
+	}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
