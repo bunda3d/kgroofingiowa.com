d6c90cf0fe94cf9e51744c4f7fe4f62e1948b80e
bunda3d <bunda3@yahoo.com>
2020-07-27 00:00:45 -0500

    more responsive flexbox to footer, contact

--- kgr/src/app/app.module.ts
+++ kgr/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatTableModule } from '@angular/material/table';
 import { MatSliderModule } from '@angular/material/slider';
@@ -15,7 +16,6 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 
 // import { OverlayContainer, FullscreenOverlayContainer } from '@angular/cdk/overlay';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
@@ -23,7 +23,7 @@ import { environment } from 'src/environments/environment';
 import { ThemeService } from './core/services/theme.service';
 import { SidenavModule } from './modules/sidenav/sidenav.module';
 import { ProjectService } from './core/services/project.service';
-
+// import { ImagekitioAngularModule } from './imagekitio-angular/imagekitio-angular.module';
 import { InputsModule } from './modules/_forms/inputs.module';
 import { EmployeesComponent } from './modules/employees/employees.component';
 import { EmployeeComponent } from './modules/employees/employee/employee.component';
@@ -40,6 +40,7 @@ import { ProjectModule } from './modules/project/project.module';
 		HammerModule,
 		HttpClientModule,
 		LayoutModule,
+		CommonModule,
 		MatSliderModule,
 		MatTableModule,
 		MatButtonModule,
@@ -51,6 +52,12 @@ import { ProjectModule } from './modules/project/project.module';
 		InputsModule,
 		ProjectModule,
 		AppRoutingModule
+		// ImagekitioAngularModule.forRoot({
+		// 	// https://github.com/imagekit-developer/imagekit-angular
+		// 	publicKey: environment.publicKey,
+		// 	urlEndpoint: environment.urlEndpoint,
+		// 	authenticationEndpoint: environment.authenticationEndpoint
+		// })
 	],
 	providers: [
 		ThemeService,
