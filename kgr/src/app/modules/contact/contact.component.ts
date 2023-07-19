d2e79d0b2b8fb1dbbd1424c4904219816381068b
bunda3d <bunda3@yahoo.com>
2023-07-19 02:26:48 -0500

    angular.material v15 breaking changes fixes 3: global and contact component

--- kgr/src/app/modules/contact/contact.component.ts
+++ kgr/src/app/modules/contact/contact.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { UntypedFormBuilder, Validators } from '@angular/forms';
-import { UntypedFormArray } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
 	selector: 'app-contact',
@@ -8,27 +6,27 @@ import { UntypedFormArray } from '@angular/forms';
 	styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
-	profileForm = this.fb.group({
-		firstName: ['', Validators.required],
-		lastName: [''],
-		address: this.fb.group({
-			street: [''],
-			city: [''],
-			state: [''],
-			zip: ['']
-		}),
+	mailtoObject: string = '';
 
-		aliases: this.fb.array([this.fb.control('')])
-	});
+	constructor() {}
 
-	constructor(private fb: UntypedFormBuilder) {}
-
-	ngOnInit(): void {}
-
-	get aliases() {
-		return this.profileForm.get('aliases') as UntypedFormArray;
-	}
-	addAlias() {
-		this.aliases.push(this.fb.control(''));
+	ngOnInit() {
+		this.mailtoObject = `mailto:kgroofing@yahoo.com
+		?subject=Email from KGRoofingIowa.com/Contact 
+		&body=
+		*** PLEASE FILL OUT THE INFO BELOW and OVERWRITE THE SAMPLE TEXT [in brackets] ***%0D%0D
+			Name: %0D
+			Phone: %0D
+			Email: %0D
+			Best Time to Contact: %0D%0D
+			ADDRESS 1: [Street] %0D
+			ADDRESS 2: [City, State, Zip] %0D
+			ROOF CLASS: [Commercial, Residential] %0D
+			ROOF TYPE: [Shingle, Metal, TPO, Rubber, etc.] %0D
+			SUBJECT: [Short Description, i.e.; Request for Free Inspection, Quote, Roofing Question, etc.] %0D
+			DESCRIPTION: [Explain your issue or request] %0D
+			%0D%0D
+			*** PLEASE ATTACH PHOTOS of your roof situation, if possible. ***%0D%0D
+		`;
 	}
 }
